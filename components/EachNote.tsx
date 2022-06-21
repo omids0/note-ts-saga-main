c0004be8a43d2ca2af2c760387c62ac6c3dd69b6
omids0 <soheilniao@gmail.com>
2022-06-21 14:24:49 +0430

    slide added for each modal

--- components/EachNote.tsx
+++ components/EachNote.tsx
@@ -1,5 +1,13 @@
+import {
+  Box,
+  Button,
+  Modal,
+  Slide,
+  TextField,
+  Typography,
+} from "@mui/material";
 import Link from "next/link";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { useDispatch } from "react-redux";
 import { INoteDataResActionType } from "../types/DTO/note";
 import { removeNoteItemActionRequest } from "./../redux/actions/notes/index";
@@ -35,15 +43,70 @@ const EachNote: FC<INoteItemProps> = (props) => {
     dispatch(removeNoteItemActionRequest(props));
   };
 
+  const [open, setOpen] = useState(false);
+
+  function handleClose() {
+    setOpen(!open);
+  }
+
+  const style = {
+    position: "absolute" as "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 500,
+    bgcolor: "background.paper",
+    border: "2px solid #eee",
+    boxShadow: 24,
+    p: 4,
+  };
+
+  const slidBox = {
+    display: "flex",
+    flexDirection: "column",
+  }
+
   return (
     <div className="each-note-container" key={rest.key}>
-      <Link href={`/edit/${id}`} passHref>
-        <div className="each-note-header">
-          <button onClick={handleRemoveItem}>X</button>
+      <div className="each-note-header">
+        <button onClick={handleRemoveItem}>X</button>
+        <Link href={`/edit/${id}`} passHref>
           <h3 className="each-note-title">{title}</h3>
-        </div>
-      </Link>
-      <div className="each-note-description">{description}</div>
+        </Link>
+      </div>
+
+      <div className="each-note-description" onClick={handleClose}>
+        {description}
+      </div>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Slide direction="up" in={open} mountOnEnter unmountOnExit>
+            <Box sx={slidBox}>
+              <TextField
+                id="outlined-basic"
+                label="Title"
+                variant="outlined"
+                value={title}
+                sx={{ margin: "10px 0", fontSize: "30px" }}
+              />
+              <TextField
+                id="outlined-basic"
+                label="Description"
+                variant="outlined"
+                value={description}
+                sx={{ marginBottom: "20px" }}
+              />
+              <Button variant="contained">Update</Button>
+            </Box>
+          </Slide>
+        </Box>
+      </Modal>
     </div>
   );
 };
